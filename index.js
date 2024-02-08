a4073712c03aade170edee0e8cc73671476122ea
AdryanPatrycky <adryanpatrycky87@gmail.com>
2024-02-08 00:06:53 -0300

    Desafio.dei.me

---
+++ index.js
@@ -0,0 +1,8 @@
+Rankeadas ("96 vitórias  ","0 derrotas ")
+
+function Rankeadas (vitórias){
+       console.log("O Herói Hulk tem um saldo de " + vitórias + "e  está no nível de  Lendário")
+
+}
+
+
